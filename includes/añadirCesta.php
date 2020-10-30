edf83c192289a736a1631f658cad2678b73dc5ba
rmesado <rafaelmesado@gmail.com>
2020-10-30 10:34:45 +0100

    si sesion en marcha, añade, sino, mensaje

--- includes/añadirCesta.php
+++ includes/añadirCesta.php
@@ -1,19 +1,20 @@
 <?php
 
 function añadirCesta(){
+    if (isset($_SESSION["usuario"])) {
     
-    if(!isset($_SESSION["cesta"])){
+    
+    if (!isset($_SESSION["cesta"])) {
         $producto=$_REQUEST["product"];
         $_SESSION["cesta"]=$producto;
-    }else{
+    } else {
         $productos=$_SESSION["cesta"];
         $producto=$_REQUEST['product'];
         $productos=$productos.",".$producto;
         $_SESSION["cesta"]=$productos;
     }
     verCesta();
-
+    }
+    else echo "<h1>Tienes que iniciar sesión antes</h1>";
 }
-
-
 ?>
