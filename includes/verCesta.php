4cd36ef71609b598bdd66a053c9e47273a482f14
rmesado <rafaelmesado@gmail.com>
2020-10-30 10:34:00 +0100

    añadido boton realizar compra

--- includes/verCesta.php
+++ includes/verCesta.php
@@ -1,14 +1,14 @@
 <?php
-    function verCesta(){
+    function verCesta()
+    {
         global $pdo;
-        if(!isset($_SESSION["cesta"])){
-           
+        if (!isset($_SESSION["cesta"])) {
             $query = "SELECT * FROM  productos;";
             $rows = $pdo->query($query)->fetchAll(\PDO::FETCH_ASSOC);
             if (is_array($rows)) {/* Creamos un listado como una tabla HTML*/
                 print '<table><thead>';
-                foreach($rows[0] as $key => $value) {
-                    if($key!="id"){
+                foreach ($rows[0] as $key => $value) {
+                    if ($key!="id") {
                         echo "<th>", $key,"</th>";
                     }
                 }
@@ -16,29 +16,25 @@
                 print "</thead>";
                 print "</table>";
             }
-        }else{
-            
+        } else {
             print'<table><thead>';
-            foreach(explode(",",$_SESSION["cesta"]) as $id_producto){
-                 
+            foreach (explode(",", $_SESSION["cesta"]) as $id_producto) {
                 $query="SELECT * FROM productos WHERE id = ".$id_producto." ;";
                 $pr= $pdo->query($query)->fetchAll(\PDO::FETCH_ASSOC);
                 $producto=$pr[0];
 
                 print "<tr>";
-                if(is_array($producto)){
-                foreach($producto as $componente){
-                    if($producto["id"]!=$componente){
-                        echo "<td>", $componente, "</td>";
-                    }                    
-                }
-                echo"<td> <a href=\"?action=delete&item_id=",$producto["id"]," \" class=\"btn btn-success\"> Borrar </a> </td>";
+                if (is_array($producto)) {
+                    foreach ($producto as $componente) {
+                        if ($producto["id"]!=$componente) {
+                            echo "<td>", $componente, "</td>";
+                        }
+                    }
+                    echo"<td> <a href=\"?action=delete&item_id=",$producto["id"]," \" class=\"btn btn-success\"> Borrar </a> </td>";
                 }
                 print "</tr>";
             }
+            echo"<section> <a href=\"?action=realizar_compra&client_id=",$_SESSION["id_usuario"]," \" class=\"btn btn-success\"> Realizar Compra </a> </section>";
             print "</table>";
         }
-
-        
     }
-?>
