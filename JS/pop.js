ccbcb52613b4692761452a11d8fdee30050d2a6a
vicoma <36210329+vicoma@users.noreply.github.com>
2020-11-15 12:32:28 +0100

    vale la cesta con js le falta cambiar los metodos para que se vea correctamente

--- JS/pop.js
+++ JS/pop.js
@@ -7,3 +7,65 @@ function fuctionhide(){
     document.getElementById("ventanita").style.visibility="hidden";
 
 }
+
+
+
+
+
+//cosas para hacer flotante el carro de manolo
+let divCaja=document.getElementById("caro_movil");
+divCaja.onmousedown=function(){startDrag();}
+
+
+function startDrag(){
+  document.onmouseup=finishDrag
+  document.onmousemove=moveElement
+}
+function moveElement(e){
+  divCaja.style.top=(divCaja.offsetTop + e.movementY) +'px';
+  divCaja.style.left=(divCaja.offsetLeft + e.movementX) +'px';
+}
+function finishDrag(e){
+  document.onmouseup = null;
+  document.onmousemove=null;
+  
+}
+
+(function(){
+    let lista = JSON.parse(localStorage.getItem('cesta'))
+    if(lista && lista.length>0)
+      lista.forEach(tarea => anyadir(tarea))
+})()
+
+function anyadir(tarea){
+  let nodo = document.createElement('li')
+  
+  let span = document.createElement('span')
+  span.classList.add('data-tarea') // añadimos una nueva clase al atributo 'class'
+
+  if (tarea) 
+     span.textContent = tarea
+  else /*si el contenido es vacio return */
+     span.textContent = document.getElementById('tarea').value
+  
+  nodo.appendChild(span)
+
+  let boton = document.createElement('button')
+  boton.textContent = 'Hecho'
+  nodo.appendChild(boton)
+  boton.onclick = eliminar
+  boton.classList.add('boton')
+
+  document.getElementById('list').appendChild(nodo)
+}
+
+function eliminar(){
+  this.parentNode.remove()
+}
+
+function guardar(){
+  let lista = document.querySelectorAll('.data-tarea')
+  //hay que conseguir que no guarde el contenido del boton
+  lista = Array.from(lista).map(n => n.textContent)
+  localStorage.setItem('cesta', JSON.stringify(lista))
+}
---
+++ partials/cestaV2.php
@@ -0,0 +1,17 @@
+<div id="caro_movil" class="widget">  
+<section class="form">
+ 
+   <input type="text" id="tarea" name="tarea" placeholder="introduce una tarea"></input>
+   <button id="envia" onclick="anyadir()">+</button>
+      
+</section>
+
+<section class="lista">
+  <ul id="list">
+  </ul>
+</section>
+
+<center>
+   <button onclick="guardar()">Guardar</button>
+</center>
+</div>
