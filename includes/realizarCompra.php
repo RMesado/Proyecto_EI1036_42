305301d31d67cd2404de008078e978437c7a6b6e
vicoma <36210329+vicoma@users.noreply.github.com>
2020-11-19 19:14:13 +0100

    ya inserta

--- includes/realizarCompra.php
+++ includes/realizarCompra.php
@@ -1,24 +1,19 @@
 <?php
     function realizarCompra($table)
     {
-        if(!isset($_SESSION["usuario"])){
+        global $pdo;
+        if(!isset($_SESSION["id_usuario"])){
             echo "<h1> inicia sesion</h1>";
         }else{
         $ids_productos=$_REQUEST["productos"];
         $ids_desconpuestos=explode(',',$ids_productos);
-        $query = "INSERT INTO $table ( id_cliente , id_producto, fecha)
-                          VALUES (?,?,?)";
         try { 
-        $consult = $pdo -> prepare($query);
-        $fecha=date("Y-m-d H:i:s");
-        foreach($ids_desconpuestos as $id){
-            $a = $consult->execute(array($_SESSION["usuario"], $id, $fecha ));
-        }
-        if (1>$a) echo "<h1> Inserción incorrecta </h1>";
-        else echo "<h1> Producto registrado! </h1>";
-
-        //$_SESSION["usuario"] = "normal"; no se que es rafa explica me lo??
-    
+            foreach($ids_desconpuestos as $id){
+                $query = "INSERT INTO $table ( id_cliente , id_producto, fecha) VALUES (".$_SESSION["id_usuario"].",".$id.",now());";
+                $pr= $pdo->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+            }
+            if (1>$pr) echo "<h1> Inserción incorrecta </h1>";
+            
     } catch (PDOExeption $e) {
         echo ($e->getMessage());
     }
